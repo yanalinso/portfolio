1af638c667f0eb4fa77b0451ad04bcac3287daef
Iana <ian_alinso@trendmicro.com>
2020-04-25 16:22:36 +0800

    Updates

---
+++ precache-manifest.cd1b0fb41d57c186636ae378de9742f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a908d15cb11eb82769e0917a4506aefa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5012db84461319fac1b1",
+    "url": "/static/js/2.ff4d75e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff4d75e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a1ccb061af88f289d1",
+    "url": "/static/js/main.3ad3f98e.chunk.js"
+  },
+  {
+    "revision": "12371047c58a498f4779",
+    "url": "/static/js/runtime-main.2101719f.js"
+  }
+]);
