fddd39094ee6c2055f7915f8ea521ddd38792e42
Iana <ian_alinso@trendmicro.com>
2020-04-25 16:25:27 +0800

    Updates

---
+++ precache-manifest.6b8481e32055cea9718aa3317bed03dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8093e37cf91238caf6907491eba2c24",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5012db84461319fac1b1",
+    "url": "/portfolio/static/js/2.ff4d75e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ff4d75e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a1ccb061af88f289d1",
+    "url": "/portfolio/static/js/main.3ad3f98e.chunk.js"
+  },
+  {
+    "revision": "da4ee72a990ebd598361",
+    "url": "/portfolio/static/js/runtime-main.3cccf8eb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cd1b0fb41d57c186636ae378de9742f5.js"
+  "/portfolio/precache-manifest.6b8481e32055cea9718aa3317bed03dd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
